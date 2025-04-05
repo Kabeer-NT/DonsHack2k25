ee1b17195f302f96b508758bffd670e9d80caa8c
zoetropew <zawong2@dons.usfca.edu>
2025-04-04 21:28:10 -0700

    adding mongodb

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,24 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
+from dotenv import load_dotenv
+import os
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+load_dotenv()
+
+uri = os.getenv("MONGODB_HOST")
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["users"]
+
+try:
+    client.admin.command('ping')
+    print("Connected to MongoDB")
+except Exception as e:
+    print(e)
 
 app = Flask(__name__)
 
