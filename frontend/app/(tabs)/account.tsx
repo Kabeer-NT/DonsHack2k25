63114434cdaa3f75497dd17890bf4f579b89bc80
zoetropew <zawong2@dons.usfca.edu>
2025-04-06 08:09:35 -0700

    friends cards

--- frontend/app/(tabs)/account.tsx
+++ frontend/app/(tabs)/account.tsx
@@ -11,202 +11,14 @@ import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
 
-const DATA: ClassData[] = [
-  {
-      "id": "1",
-      "class_id": "CS110",
-      "name": "Intro to CS I",
-      "professor": "Andrew Rothman",
-      "professor_image": "url",
-      "location": "Harney 148",
-      "schedule": "MW 4:45pm-6:25pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "2",
-      "class_id": "CS110",
-      "name": "Intro to CS I",
-      "professor": "Andrew Rothman",
-      "professor_image": "url",
-      "location": "Harney 148",
-      "schedule": "MW 6:30pm-8:15pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "3",
-      "class_id": "CS110",
-      "name": "Intro to CS I",
-      "professor": "Nancy Stevens",
-      "professor_image": "url",
-      "location": "Harney 148",
-      "schedule": "MW 11:15am-1:00pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "4",
-      "class_id": "CS110",
-      "name": "Intro to CS I",
-      "professor": "Kelsey Urgo",
-      "professor_image": "url",
-      "location": "Lo Schiavo 307",
-      "schedule": "TR 9:55am-11:40am",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "5",
-      "class_id": "CS110",
-      "name": "Intro to CS I",
-      "professor": "Julia Norfo",
-      "professor_image": "url",
-      "location": "Lo Schiavo G12",
-      "schedule": "TR 2:40pm-4:25pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "6",
-      "class_id": "CS111",
-      "name": "Foundations of Program Design",
-      "professor": "Edward Reese",
-      "professor_image": "url",
-      "location": "Lo Schiavo 307",
-      "schedule": "MW 12:10pm-1:55pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "7",
-      "class_id": "CS112",
-      "name": "Intro to CS II",
-      "professor": "Alark Joshi",
-      "professor_image": "url",
-      "location": "Lo Schiavo G12",
-      "schedule": "TR 9:55am-11:40am",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "8",
-      "class_id": "CS221",
-      "name": "C and Systems Programming",
-      "professor": "Paul Haskell",
-      "professor_image": "url",
-      "location": "Harney 148",
-      "schedule": "MW 9:20am-11:05am",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "9",
-      "class_id": "CS221",
-      "name": "C and Systems Programming",
-      "professor": "Paul Haskell",
-      "professor_image": "url",
-      "location": "Harney 148",
-      "schedule": "MW 1:10pm-2:55pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "10",
-      "class_id": "CS245",
-      "name": "Data Structures and Algorithms",
-      "professor": "David Guy Brizan",
-      "professor_image": "url",
-      "location": "Education 103",
-      "schedule": "MWF 11:45am-12:50pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "11",
-      "class_id": "CS256",
-      "name": "Career Prep",
-      "professor": "Jon Rahoi",
-      "professor_image": "url",
-      "location": "Lo Schiavo 307",
-      "schedule": "R 6:30pm-8:15pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "12",
-      "class_id": "CS272",
-      "name": "Software Development",
-      "professor": "Philip Peterson",
-      "professor_image": "url",
-      "location": "Harney 148",
-      "schedule": "TR 8:00am-9:45am",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "13",
-      "class_id": "CS315",
-      "name": "Computer Architecture",
-      "professor": "Gregory Benson",
-      "professor_image": "url",
-      "location": "Lo Schiavo 307",
-      "schedule": "TR 8:00am-9:45am",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "14",
-      "class_id": "CS345",
-      "name": "Programming Language Paradigms",
-      "professor": "Kristin Jones",
-      "professor_image": "url",
-      "location": "Education 103",
-      "schedule": "MWF 9:15am-10:20am",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  },
-  {
-      "id": "15",
-      "class_id": "CS490",
-      "name": "Senior Team",
-      "professor": "Paul Haskell",
-      "professor_image": "url",
-      "location": "Lone Mountain Main 350",
-      "schedule": "MW 4:45pm-6:25pm",
-      "students_taking": [],
-      "students_interested": [],
-      "num_students": 0
-  }
-]
 const EMPTY_DATA: ClassData[] = []; // Used for testing no items to display message
 
 // Structure of a class
 type ClassData = {
-  id: string;
-  class_id: string;
-  name: string;
-  professor: string;
-  professor_image: string;
-  location: string;
-  schedule: string;
-  students_taking: [];
-  students_interested: [];
-  num_students: number;
+  _id: string;
+  clerk_id: string;
+  username: string;
+  pfp: string;
 };
 
 // Properties of a class
@@ -222,7 +34,7 @@ const Class = ({item, onPress, item_style, text_style}: ClassProps) => (
   <TouchableOpacity onPress={onPress} style={item_style}>
     <ThemedView style={item_style}>
       <Image
-        source={{uri: item.professor_image}}
+        source={{uri: item.pfp}}
         style={{
           width: 50,
           height: 50,
@@ -230,12 +42,7 @@ const Class = ({item, onPress, item_style, text_style}: ClassProps) => (
           marginRight: 12,
         }}
       />
-      <Text style={[text_style, {fontWeight: 'bold'}]}>{item.class_id}:</Text>
-      <Text style={[text_style, {fontWeight: 'bold'}]}>{item.name}</Text>
-
-      <Text style={text_style}>{item.professor}</Text>
-      <Text style={text_style}>{item.schedule}</Text>
-      <Text style={text_style}>{item.location}</Text>
+      <Text style={[text_style, {fontWeight: 'bold', textAlign: 'center'}]}>{item.username}</Text>
     </ThemedView>
   </TouchableOpacity>
 );
